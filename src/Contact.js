3e794f8bf7a5da3fd84fc17ecdb0b7b1ea680aab
sarang <gakharsarang1@gmail.com>
2022-10-11 00:40:13 +0530

    made some changes in contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,33 +1,38 @@
 import React from 'react'
-import Nav from "./NavBar"
-
+// import Nav from "./NavBar"
+import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
+
 export default function  Contact() {
-  return (
-    <>
-    <Form>
+  
+    
+
+return(
+     <Form style={{paddingTop:'20px',marginLeft:'20px'}}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label><h3>Email address</h3></Form.Label>
+        <Form.Control type="email" style={{width:'50vw'}} placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Label ><h3>Type Your Query</h3></Form.Label>
+        <Form.Control type="text" style={{width:'50vw',height:'100px'}} placeholder="" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3" controlId="formBasicCheckbox" >
+        <Form.Check type="checkbox" label="subscribe"  />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>
-    </>
-  )
+    </Form> 
+
+
+)
+  
 }
