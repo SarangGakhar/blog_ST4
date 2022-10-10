8d9f686114fc1355f0d6d61dc0a4a5fc3321f220
sarang <gakharsarang1@gmail.com>
2022-10-10 19:57:37 +0530

    made some changes in about

--- src/About.js
+++ src/About.js
@@ -1,18 +1,26 @@
 import {Link} from "react-router-dom"
 import { useParams } from "react-router-dom"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function About(){
     
    
     return (
         <>
         
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://raw.githubusercontent.com/SarangGakhar/image-gallery/master/sarang.jpg" />
+      <Card.Body>
+        <Card.Title>Sarang</Card.Title>
+        <Card.Text>
+          Hi i am sarang i am a web developer currently studying in 5th semester in Chitkara University 
+        </Card.Text>
+        <Button variant="primary">Resume</Button>
+      </Card.Body>
+    </Card>
         
-        <div>
-
-            <p>Hi my name is sarang </p>
-          
-        </div>
-
+        
         </>
     );
 }
